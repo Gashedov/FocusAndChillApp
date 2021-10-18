58e236242d467f538663875749f90817d098432b
Artem <osiuk@belaz.minsk.by>
2021-10-18 19:17:59 +0300

    done with music playing

---
+++ FocOn/Presentation/Utils/CustomSlider.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSlider.swift
+//  FocOn
+//
+//  Created by Artem Gorshkov on 28.09.21.
+//
+
+import Foundation
---
+++ FocOn/Service/AudioPlayer.swift
@@ -0,0 +1,8 @@
+//
+//  AudioPlayer.swift
+//  FocOn
+//
+//  Created by Artem Gorshkov on 1.10.21.
+//
+
+import Foundation
