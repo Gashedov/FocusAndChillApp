eea197d5b17ea94df6dbc60267dc802bd59f1c5c
Nikita Zhukov <nikita.zhukov@devexperts.com>
2021-10-24 21:14:27 +0300

    make .gitignore to work

--- Pods/Target Support Files/Pods-FocOn-FocOnUITests/Pods-FocOn-FocOnUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FocOn_FocOnUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FocOn_FocOnUITests
-@end
--- Pods/Target Support Files/Pods-FocOnTests/Pods-FocOnTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FocOnTests : NSObject
-@end
-@implementation PodsDummy_Pods_FocOnTests
-@end
